5a2e502b494406042f3e5ed2a345cf2d8338fe4a
InakiDavila <75996220+InakiDavila@users.noreply.github.com>
2023-09-14 12:19:52 -0300

    Add files via upload

---
+++ proyecto_tic/src/main/java/com/example/landing_controller.java
@@ -0,0 +1,46 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextField;
+
+public class landing_controller {
+
+    @FXML
+    private MenuItem action2_bttn;
+
+    @FXML
+    private MenuItem action3_bttn;
+
+    @FXML
+    private TextField cantidad_field;
+
+    @FXML
+    private TextField destino_field;
+
+    @FXML
+    private MenuItem misvuelos_button;
+
+    @FXML
+    private MenuButton operaciones_menu;
+
+    @FXML
+    private TextField origen_field;
+
+    @FXML
+    private Button reservar_button;
+
+    @FXML
+    private DatePicker retorno_field;
+
+    @FXML
+    private DatePicker salida_field;
+
+    @FXML
+    void login_try(ActionEvent event) {
+
+    }
+
+}
