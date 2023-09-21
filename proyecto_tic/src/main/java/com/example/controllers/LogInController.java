014cf40f080560b1b0b9fc71a952eba043d5ac29
InakiDavila <75996220+InakiDavila@users.noreply.github.com>
2023-09-21 11:28:41 -0300

    Create LogInController.java

---
+++ proyecto_tic/src/main/java/com/example/controllers/LogInController.java
@@ -0,0 +1,31 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class logincontroller {
+
+    @FXML
+    private PasswordField contrasena_field;
+
+    @FXML
+    private Button login_button;
+
+    @FXML
+    private Button signin_view_button;
+
+    @FXML
+    private TextField usuario_field;
+
+    @FXML
+    void login_try(ActionEvent event) {
+
+    }
+
+    @FXML
+    void signin_view(ActionEvent event) {
+
+    }
+
+}
