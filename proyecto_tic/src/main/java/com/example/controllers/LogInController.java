803f794a29bef001fcb7b202a39bd9390818c40e
agusalvarez6 <131990081+agusalvarez6@users.noreply.github.com>
2023-09-21 11:47:59 -0300

    nidea

--- proyecto_tic/src/main/java/com/example/controllers/LogInController.java
+++ proyecto_tic/src/main/java/com/example/controllers/LogInController.java
@@ -1,12 +1,17 @@
+package com.example.controllers;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import java.io.IOException;
 
-public class logincontroller {
+public class LogInController {
 
     @FXML
     private PasswordField contrasena_field;
