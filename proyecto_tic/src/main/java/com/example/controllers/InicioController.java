440de36e66fe18a6df5b55e4238192613110c930
InakiDavila <75996220+InakiDavila@users.noreply.github.com>
2023-09-28 10:19:55 -0300

    Add files via upload

---
+++ proyecto_tic/src/main/java/com/example/controllers/InicioController.java
@@ -0,0 +1,12 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class InicioController {
+
+    @FXML
+    private Label Registrarse_label;
+
+    @FXML
+    private Label ingresar_label;
+
+}
