2cc4c048f8b49f2029ad07dacc11a0867084c65b
InakiDavila <75996220+InakiDavila@users.noreply.github.com>
2023-09-28 10:36:21 -0300

    Update InicioController.java

--- proyecto_tic/src/main/java/com/example/controllers/InicioController.java
+++ proyecto_tic/src/main/java/com/example/controllers/InicioController.java
@@ -9,4 +9,39 @@ public class InicioController {
     @FXML
     private Label ingresar_label;
 
+    @FXML
+    void GoToLogIn(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/login.fxml"));
+            Parent logInRoot = loader.load();
+
+            Stage logInStage = new Stage();
+            logInStage.setTitle("Log In"); // Set a title for the new window
+
+            Scene logInScene = new Scene(logInRoot);
+
+            logInStage.setScene(logInScene);
+            logInStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    void GoToSignIn(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/signIn.fxml"));
+            Parent signInRoot = loader.load();
+
+            Stage signInStage = new Stage();
+            signInStage.setTitle("Sign In"); // Set a title for the new window
+
+            Scene signInScene = new Scene(signInRoot);
+
+            signInStage.setScene(signInScene);
+            signInStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
