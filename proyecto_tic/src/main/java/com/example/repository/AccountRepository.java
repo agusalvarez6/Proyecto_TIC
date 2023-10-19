e9d44c6462571536c9076ea6fd86b38ce1b72b4e
Tomy1303 <131986315+Tomy1303@users.noreply.github.com>
2023-10-19 10:58:37 -0300

    Update AccountRepository.java

--- proyecto_tic/src/main/java/com/example/repository/AccountRepository.java
+++ proyecto_tic/src/main/java/com/example/repository/AccountRepository.java
@@ -1,8 +1,20 @@
 package com.example.repository;
 
-import com.example.entities.Account;
+import com.example.entities.*;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+@Repository
 public interface AccountRepository extends JpaRepository<Account,Long>{
-
+}
+@Repository
+public interface AirlineRepository extends JpaRepository<Airline, Long> {
+}
+@Repository
+public interface AirportRepository extends JpaRepository<Airport, Long> {
+}
+@Repository
+public interface AirportEmployeeRepository extends JpaRepository<AirportEmployee, Long> {
+}
+@Repository
+public interface ShopRepository extends JpaRepository<Shop, Long> {
 }
