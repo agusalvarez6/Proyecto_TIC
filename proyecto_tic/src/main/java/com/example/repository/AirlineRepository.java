57cbd06a83549a05a928d7fe618dcb8d560095ca
pipeham-s <pipeham03@gmail.com>
2023-10-19 11:15:45 -0300

    repoa

--- proyecto_tic/src/main/java/com/example/repository/AirlineRepository.java
+++ proyecto_tic/src/main/java/com/example/repository/AirlineRepository.java
@@ -5,6 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AccountRepository extends JpaRepository<Account,Long>{
-    Account findByUsername(String username);
+public interface AirlineRepository extends JpaRepository<Airline, Long> {
 }
