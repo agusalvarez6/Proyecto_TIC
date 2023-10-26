97ba5e267a58c398553a0d3b3a67935dfceb6c62
Tomy1303 <131986315+Tomy1303@users.noreply.github.com>
2023-10-26 12:04:19 -0300

    Delete proyecto_tic/src/main/java/com/example/repository/TypePlaneRepository.java

--- proyecto_tic/src/main/java/com/example/repository/TypePlaneRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.repository;
-
-import com.example.entities.*;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TypePlaneRepository extends JpaRepository<TypePlane, Long> {
-}
