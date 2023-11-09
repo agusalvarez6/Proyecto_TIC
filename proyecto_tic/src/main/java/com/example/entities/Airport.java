b9c564d0cacd257b091a4aaac0ae60d79ceaaf9d
Tomy1303 <131986315+Tomy1303@users.noreply.github.com>
2023-11-09 10:14:18 -0300

    Update Airport.java

--- proyecto_tic/src/main/java/com/example/entities/Airport.java
+++ proyecto_tic/src/main/java/com/example/entities/Airport.java
@@ -5,14 +5,45 @@ import jakarta.persistence.*;
 @Entity
 public class Airport {
     @Id
-    private Long IATA;
+    private String iATA;
     @Column
-    private String Name;
+    private String name;
     @Column
-    private String Location;
+    private String location;
     @Column
-    private int JetBridgeNum;
+    private int jetBridgeNum;
     @Column
-    private Long IdUser;
+    private Long idUser;
 
+    public String getIATA(){
+        return this.iATA
+    }
+    public void setIATA(String IATA){
+        this.iATA = IATA
+    }
+    public String getName(){
+        return this.name 
+    }
+    public void setName(String Name){
+        this.name = Name
+    }
+    public String getLocation(){
+        return this.location
+    }
+    public void setLocation(String Location){
+        this.location = Location
+    }
+    public int getJetBridgeNum(){
+        return this.jetBridgeNum 
+    }
+    public void setJetBridgeNum(int JetBridgeNum){
+        this.jetBridgeNum = JetBridgeNum
+    }
+    public Long getIdUser(){
+        return this.idUser 
+    }
+    public void setIdUser(Long IdUser){
+        this.idUser = IdUser
+    }
 }
+
